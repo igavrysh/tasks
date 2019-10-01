6783ead705ba2d02489753c1bc302ad998688be4
ievgen_gavrysh <ievgen.gavrysh@gmail.com>
2019-09-30 23:43:01 -0700

    Add update task

--- src/main/frontend/src/app/tasks/tasks.service.ts
+++ src/main/frontend/src/app/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map } from "rxjs/operators";
+import {Task} from "./task.model";
 
 @Injectable()
 export class TasksService {
@@ -10,5 +11,10 @@ export class TasksService {
 
   getTasks() {
     return this.http.get("/api/tasks").pipe();
+  }
+
+  saveTask(task: Task, checked: boolean) {
+    task.completed = checked;
+    return this.http.post('/api/tasks/save', task).pipe();
   }
 }
